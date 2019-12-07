3b723a7be9b58251112f3f972d2f372fe999cae9
Luke Morton <lukemorton.dev@gmail.com>
2019-12-07 08:44:16 +0000

    Improve message to channel

--- clean-architecture/using-clojure/src/eng_bot/share_channels.clj
+++ clean-architecture/using-clojure/src/eng_bot/share_channels.clj
@@ -1,15 +1,24 @@
 (ns eng-bot.share-channels
   (:require [clojure.string :refer [join]]))
 
-(defn- channel-list-message-text
+(defn- channel-list-text
   [channels]
   (->> channels
        (map #(get % :name))
        (map #(str "#" %))
        (join ", ")))
 
+(defn- message-text
+  [channels]
+  (format (str ":hammer_and_wrench: :hammer_and_wrench:\n"
+               "Hi fellow engineers! Gee have I got some slack channels for you. "
+               "There's nothing like sharing what you know and leaning on others for help. "
+               "Why not join and participate in a few engineering channels?\n\n"
+               "%s")
+          (channel-list-text channels)))
+
 (defn exec
   [list-channels post-message]
   (->> (list-channels)
-       (channel-list-message-text)
+       (message-text)
        (post-message "eng-test")))
