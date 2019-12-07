bbbcb0bd546de8a81127c8307a0f8d05581f2974
Luke Morton <lukemorton.dev@gmail.com>
2019-12-07 13:11:11 +0000

    Mock call to use case

--- clean-architecture/using-clojure/test/eng_bot/cli_test.clj
+++ clean-architecture/using-clojure/test/eng_bot/cli_test.clj
@@ -1,7 +1,11 @@
 (ns eng-bot.cli-test
-  (:require [eng-bot.cli :as cli]))
+  (:require [eng-bot.cli :as cli]
+            [eng-bot.factory :as factory]
+            [spy.core :refer [spy called-once?]]))
 
 (use 'clojure.test)
 
 (deftest calls-share-channels
-  (is (not= nil (cli/-main "share-channels"))))
+  (with-redefs [factory/share-channels (spy)]
+    (cli/-main "share-channels")
+    (is (called-once? factory/share-channels))))
