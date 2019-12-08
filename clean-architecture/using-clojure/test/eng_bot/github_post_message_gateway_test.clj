22b527217151836071cf6d6bc3610af51b507302
Luke Morton <lukemorton.dev@gmail.com>
2019-12-08 11:23:12 +0000

    Add more tests

--- clean-architecture/using-clojure/test/eng_bot/github_post_message_gateway_test.clj
+++ clean-architecture/using-clojure/test/eng_bot/github_post_message_gateway_test.clj
@@ -1,6 +1,6 @@
 (ns eng-bot.github-post-message-gateway-test
   (:require [clojure.test :refer [deftest]]
-            [expectations.clojure.test :refer [expect]]
+            [expectations.clojure.test :refer [expecting expect]]
             [stub-http.core :as stub-http]
             [eng-bot.test-util.routes :as routes]
             [eng-bot.github-post-message-gateway :as g]))
@@ -8,4 +8,8 @@
 (deftest post-message-test
   (stub-http/with-routes!
     routes/slack-post-message
-    (expect true (g/post-message {:api-url uri :token "test"} "eng-test" "HeY!"))))
+    (expecting "return true on success"
+      (expect true (g/post-message {:api-url uri :token "test"} "eng-test" "HeY!")))
+    (expecting "sends a single POST request to Slack"
+      (expect 1 (count (stub-http/recorded-requests server)))
+      (expect "POST" (:method (first (stub-http/recorded-requests server)))))))
