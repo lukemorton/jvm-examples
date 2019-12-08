81e0200324623d8a55973b5036f35f00fa892bf6
Luke Morton <lukemorton.dev@gmail.com>
2019-12-08 11:06:16 +0000

    Restructure tests for share-channels

--- clean-architecture/using-clojure/test/eng_bot/share_channels_test.clj
+++ clean-architecture/using-clojure/test/eng_bot/share_channels_test.clj
@@ -1,6 +1,6 @@
 (ns eng-bot.share-channels-test
   (:require [clojure.test :refer [deftest]]
-            [expectations.clojure.test :refer [expect]]
+            [expectations.clojure.test :refer [expect expecting]]
             [clojure.spec.alpha :as s]
             [spy.core :refer [spy called-once? calls]]
             [eng-bot.share-channels :as sc]))
@@ -20,21 +20,22 @@
   [channel text]
   true)
 
-(deftest exec-returns-true-on-success
-  (expect true (sc/exec fake-list-channels fake-post-message)))
-
-(deftest exec-returns-true-even-with-empty-channels
-  (expect true (sc/exec fake-empty-list-channels fake-post-message)))
-
-(deftest exec-calls-list-channels
-  (let [fake-list-channels-spy (spy fake-list-channels)]
-    (sc/exec fake-list-channels-spy fake-post-message)
-    (expect called-once? fake-list-channels-spy)))
-
 (s/def ::post-message-call (s/cat :channel string? :text string?))
 
-(deftest exec-calls-post-message
-  (let [fake-post-message-spy (spy fake-post-message)]
-    (sc/exec fake-list-channels fake-post-message-spy)
-    (expect called-once? fake-post-message-spy)
-    (expect ::post-message-call (first (calls fake-post-message-spy)))))
+(deftest exec-test
+  (expecting "returns true on success"
+    (expect true (sc/exec fake-list-channels fake-post-message)))
+
+  (expecting "returns true even with empty channels"
+    (expect true (sc/exec fake-empty-list-channels fake-post-message)))
+
+  (expecting "calls list-channels"
+    (let [fake-list-channels-spy (spy fake-list-channels)]
+      (sc/exec fake-list-channels-spy fake-post-message)
+      (expect called-once? fake-list-channels-spy)))
+
+  (expecting "calls post-message"
+    (let [fake-post-message-spy (spy fake-post-message)]
+      (sc/exec fake-list-channels fake-post-message-spy)
+      (expect called-once? fake-post-message-spy)
+      (expect ::post-message-call (first (calls fake-post-message-spy))))))
