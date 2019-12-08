146db1d96ba5bd58488f227afe38dd16a650829c
Luke Morton <lukemorton.dev@gmail.com>
2019-12-08 11:29:34 +0000

    Add more comprehensive tests

--- clean-architecture/using-clojure/test/eng_bot/github_channel_gateway_test.clj
+++ clean-architecture/using-clojure/test/eng_bot/github_channel_gateway_test.clj
@@ -1,6 +1,6 @@
 (ns eng-bot.github-channel-gateway-test
   (:require [clojure.test :refer [deftest]]
-            [expectations.clojure.test :refer [expect]]
+            [expectations.clojure.test :refer [expecting expect]]
             [clojure.spec.alpha :as s]
             [stub-http.core :as stub-http]
             [eng-bot.test-util.routes :as routes]
@@ -9,15 +9,31 @@
 (s/def ::expected-channels (s/coll-of #(s/valid? ::g/channels %) :min-count 2 :max-count 2))
 
 (deftest all-channels-test
-  (stub-http/with-routes!
-    routes/slack-channels
-    (let [channels (g/all-channels {:api-url uri :token "test"})]
-      (expect ::expected-channels channels))))
+  (expecting "sends single GET request to Slack"
+    (stub-http/with-routes!
+      routes/slack-channels
+      (g/all-channels {:api-url uri :token "test"})
+      (expect 1 (count (stub-http/recorded-requests server)))
+      (expect "GET" (:method (first (stub-http/recorded-requests server))))))
+
+  (expecting "channels conform to spec"
+    (stub-http/with-routes!
+      routes/slack-channels
+      (let [channels (g/all-channels {:api-url uri :token "test"})]
+        (expect ::expected-channels channels)))))
 
 (s/def ::expected-messages (s/coll-of #(s/valid? ::g/message %) :min-count 100 :max-count 100))
 
 (deftest channel-messages-test
-  (stub-http/with-routes!
-    routes/slack-channel-history
-    (let [messages (g/channel-messages {:api-url uri :token "test"} "eng-general")]
-      (expect ::expected-messages messages))))
+  (expecting "sends single GET request to Slack"
+    (stub-http/with-routes!
+      routes/slack-channel-history
+      (g/channel-messages {:api-url uri :token "test"} "eng-general")
+      (expect 1 (count (stub-http/recorded-requests server)))
+      (expect "GET" (:method (first (stub-http/recorded-requests server))))))
+
+  (expecting "messages to conform to spec"
+    (stub-http/with-routes!
+      routes/slack-channel-history
+      (let [messages (g/channel-messages {:api-url uri :token "test"} "eng-general")]
+        (expect ::expected-messages messages)))))
--- clean-architecture/using-clojure/test/eng_bot/github_post_message_gateway_test.clj
+++ clean-architecture/using-clojure/test/eng_bot/github_post_message_gateway_test.clj
@@ -6,10 +6,14 @@
             [eng-bot.github-post-message-gateway :as g]))
 
 (deftest post-message-test
-  (stub-http/with-routes!
-    routes/slack-post-message
-    (expecting "return true on success"
-      (expect true (g/post-message {:api-url uri :token "test"} "eng-test" "HeY!")))
-    (expecting "sends a single POST request to Slack"
+  (expecting "return true on success"
+    (stub-http/with-routes!
+      routes/slack-post-message
+      (expect true (g/post-message {:api-url uri :token "test"} "eng-test" "HeY!"))))
+
+  (expecting "sends a single POST request to Slack"
+    (stub-http/with-routes!
+      routes/slack-post-message
+      (g/post-message {:api-url uri :token "test"} "eng-test" "HeY!")
       (expect 1 (count (stub-http/recorded-requests server)))
       (expect "POST" (:method (first (stub-http/recorded-requests server)))))))
