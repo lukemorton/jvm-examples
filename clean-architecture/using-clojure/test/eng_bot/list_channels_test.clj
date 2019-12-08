574163d63318b9ca42d8e325502bbd8ef2e1cdb2
Luke Morton <lukemorton.dev@gmail.com>
2019-12-08 11:02:49 +0000

    Restructure tests for list-channels

--- clean-architecture/using-clojure/test/eng_bot/list_channels_test.clj
+++ clean-architecture/using-clojure/test/eng_bot/list_channels_test.clj
@@ -1,6 +1,6 @@
 (ns eng-bot.list-channels-test
   (:require [clojure.test :refer [deftest]]
-            [expectations.clojure.test :refer [expect]]
+            [expectations.clojure.test :refer [expect expecting]]
             [eng-bot.list-channels :as lc]))
 
 (defn- fake-empty-gateway
@@ -17,24 +17,25 @@
    {:name "engagements"}
    {:name "team-sales"}])
 
-(deftest exec-returns-empty-list-from-empty-gateway
-  (expect empty? (lc/exec fake-empty-gateway)))
+(deftest exec-test
+  (expecting "returns empty list from empty gateway"
+    (expect empty? (lc/exec fake-empty-gateway)))
 
-(deftest exec-returns-list-from-none-empty-gateway
-  (expect seq (lc/exec fake-gateway)))
+  (expecting "returns list from none empty gateway"
+    (expect seq (lc/exec fake-gateway)))
 
-(deftest exec-only-returns-eng-channels
-  (let [channel-names (map #(get % :name) (lc/exec fake-gateway))]
-    (expect "eng-general" (in channel-names))
-    (expect "eng-java" (in channel-names))
-    (expect "eng-dotnet" (in channel-names))
-    (expect nil? (some #{"announcements"} channel-names))
-    (expect nil? (some #{"engagements"} channel-names))
-    (expect nil? (some #{"team-sales"} channel-names))))
+  (expecting "returns only engineering channels"
+    (let [channel-names (map #(get % :name) (lc/exec fake-gateway))]
+      (expect "eng-general" (in channel-names))
+      (expect "eng-java" (in channel-names))
+      (expect "eng-dotnet" (in channel-names))
+      (expect nil? (some #{"announcements"} channel-names))
+      (expect nil? (some #{"engagements"} channel-names))
+      (expect nil? (some #{"team-sales"} channel-names))))
 
-(deftest exec-sorts-channel-names-with-general-first-and-then-alphabetical
-  (let [channel-names (mapv #(get % :name) (lc/exec fake-gateway))]
-    (expect "eng-general" (get channel-names 0))
-    (expect "eng-dotnet" (get channel-names 1))
-    (expect "eng-java" (get channel-names 2))
-    (expect "eng-ruby" (get channel-names 3))))
+  (expecting "returns channels sorted by general first and then alphabetical"
+    (let [channel-names (mapv #(get % :name) (lc/exec fake-gateway))]
+      (expect "eng-general" (get channel-names 0))
+      (expect "eng-dotnet" (get channel-names 1))
+      (expect "eng-java" (get channel-names 2))
+      (expect "eng-ruby" (get channel-names 3)))))
