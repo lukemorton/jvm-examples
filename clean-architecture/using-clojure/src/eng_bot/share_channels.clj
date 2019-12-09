9763cab9d1e1fe4c981c1017bf800b9672c888a6
Luke Morton <lukemorton.dev@gmail.com>
2019-12-09 10:20:34 +0000

    Improve message

--- clean-architecture/using-clojure/src/eng_bot/share_channels.clj
+++ clean-architecture/using-clojure/src/eng_bot/share_channels.clj
@@ -12,17 +12,16 @@
 (defn- hot-channel-list-text
   [hot-channels]
   (->> hot-channels
-       (map #(format "- #%s with %s messages this week" (get % ::c/name) (get % ::c/recent-message-count)))
+       (map #(format "- #%s with %s messages" (get % ::c/name) (get % ::c/recent-message-count)))
        (join "\n")))
 
 (defn- message-text
   [channels hot-channels]
-  (format (str ":hammer_and_wrench: :hammer_and_wrench:\n"
-               "Hi fellow engineers! Gee have I got some slack channels for you. "
-               "There's nothing like sharing what you know and leaning on others for help. "
+  (format (str "Hi fellow engineers! Gee have I got some slack channels for you. "
+               "There's nothing like sharing what you know and learning from others. "
                "Why not join and participate in a few engineering channels?\n\n"
                "%s\n\n"
-               "There are three hot channels right now:\n"
+               "Last weeks hottest channels:\n"
                "%s")
           (channel-list-text channels)
           (hot-channel-list-text hot-channels)))
